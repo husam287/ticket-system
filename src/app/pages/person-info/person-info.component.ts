6e53393cfce051a2915fd6621dd44c1c960f5da5
Hossam Sherif <husam287@hotmail.com>
2021-07-29 11:47:21 +0200

    add person-info page

--- src/app/pages/person-info/person-info.component.ts
+++ src/app/pages/person-info/person-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-person-info',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonInfoComponent implements OnInit {
 
+  /* Years Stuff */
+  years = [];
+  yearMaxLimit = 2013;
+  yearMinLimit = 1910;
+
+  /* T-shirt Stuff */
+  sizes = ['xs','s','m','l','xl','xxl']
+  
+  /* Promo Code */
+  havePromo = false;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.makeArrayOfYears();
   }
+
+  onSubmit(form: NgForm) {
+
+    console.log(`form2`, form);
+  }
+
+  /* Fill Years Array */
+  private makeArrayOfYears() {
+    for (let year = this.yearMaxLimit; year >= this.yearMinLimit; year--)
+      this.years.push(year);
+  }
+
 
 }
