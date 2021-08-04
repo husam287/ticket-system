b2b10e7f2e8ac51120928b50705280a286133147
Hossam Sherif <husam287@hotmail.com>
2021-08-04 13:04:09 +0200

    validators added form is preservable now

--- src/app/pages/person-info/person-info.component.ts
+++ src/app/pages/person-info/person-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FormService } from 'src/app/providers/form.service';
@@ -27,24 +27,47 @@ export class PersonInfoComponent implements OnInit {
   currentNumberTicket = 1;
   tickets = [];
 
+  /* Error Flags */
+  Error_Flag = false;
+  ERROR_INPUT_CLASS = "border border-danger"
+
+  /* Form */
+  initValue = {}
+  @ViewChild('headerOfTheForm') headerOfTheForm: ElementRef<HTMLElement>;
+
   constructor(private router: Router, private formService: FormService) { }
 
   ngOnInit(): void {
-    this.formService.clearTickets();
-    
     this.makeArrayOfYears();
     this.totalNumberOfMembers = this.formService.form['members'];
     this.currentNumberTicket = this.formService.form['members'];
+    this.initValue = this.formService.form['tickets'] ? this.formService.form['tickets'][0] : {};
+    console.log("init", this.initValue)
   }
 
   onSubmit(form: NgForm) {
+    if (!form.valid) {
+      this.Error_Flag = true;
+      this.headerOfTheForm.nativeElement.scrollIntoView({ behavior: 'smooth' })
+      return;
+    }
+    this.Error_Flag = false;
+
     this.currentNumberTicket--;
     this.tickets.push(form.value)
-    form.resetForm();
+
+    let ticketIndex = this.totalNumberOfMembers - this.currentNumberTicket;
+    this.initValue = this.formService.form['tickets'] &&
+      this.formService.form['tickets'].length - 1 >= ticketIndex &&
+      this.formService.form['tickets'][ticketIndex];
+    if (!this.initValue) this.initValue = {}
+    form.resetForm(this.initValue);
+
+    this.headerOfTheForm.nativeElement.scrollIntoView({ behavior: 'smooth' })
 
     if (this.currentNumberTicket === 0) {
       this.formService.addToForm({ tickets: this.tickets })
-      console.log("Form Now",this.formService.form)
+      console.log("Form Now", this.formService.form)
       this.router.navigateByUrl('/confirmation')
     }
   }
--- src/app/pages/ticket-info/ticket-info.component.ts
+++ src/app/pages/ticket-info/ticket-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FormService } from 'src/app/providers/form.service';
@@ -11,13 +11,25 @@ import { TicketsService } from 'src/app/providers/tickets.service';
 })
 export class TicketInfoComponent implements OnInit {
 
+  /* Error Flags */
+  Error_Flag = false;
+  ERROR_INPUT_CLASS = "border border-danger"
+
+  /* Form */
+  initValues = {}
+
   constructor(private router:Router, private formService:FormService, private ticketService:TicketsService) { }
 
   ngOnInit(): void {
+    this.initValues = this.formService.form;
   }
 
   onSubmit(form:NgForm){
-    
+    if(!form.valid){
+      this.Error_Flag = true;
+      return ;
+    }
+
     this.formService.addToForm(form.value);
 
     let price = this.ticketService.getTicketPrice(form.value.raceType)
